efd2f5eb4a3e27fc0ffa87141ecb3c5490cb019c
Latr21 »git config --global user.email « latourchristianpro@gmail.com <latr@LATR-2.local>
2025-03-03 16:30:21 +0100

    ajout_image_refonte

--- src/Controller/ChantierController.php
+++ src/Controller/ChantierController.php
@@ -9,12 +9,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/chantier')]
-final class ChantierController extends AbstractController
+class ChantierController extends AbstractController
 {
-    #[Route(name: 'app_chantier_index', methods: ['GET'])]
+    #[Route('/', name: 'app_chantier_index', methods: ['GET'])]
     public function index(ChantierRepository $chantierRepository): Response
     {
         return $this->render('chantier/index.html.twig', [
@@ -23,22 +26,43 @@ final class ChantierController extends AbstractController
     }
 
     #[Route('/new', name: 'app_chantier_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $chantier = new Chantier();
         $form = $this->createForm(ChantierType::class, $chantier);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Gérer l'upload d'image
+            /** @var UploadedFile $imageFile */
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Erreur lors de l\'upload de l\'image.');
+                }
+
+                $chantier->setImage($newFilename);
+            }
+
             $entityManager->persist($chantier);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_chantier_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', 'Chantier créé avec succès !');
+            return $this->redirectToRoute('app_chantier_index');
         }
 
         return $this->render('chantier/new.html.twig', [
             'chantier' => $chantier,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -51,31 +75,69 @@ final class ChantierController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_chantier_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Chantier $chantier, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Chantier $chantier, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ChantierType::class, $chantier);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $imageFile */
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+
+                    // Supprimer l'ancienne image si elle existe
+                    if ($chantier->getImage()) {
+                        $oldImagePath = $this->getParameter('images_directory') . '/' . $chantier->getImage();
+                        if (file_exists($oldImagePath)) {
+                            unlink($oldImagePath);
+                        }
+                    }
+
+                    $chantier->setImage($newFilename);
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Erreur lors de l\'upload de l\'image.');
+                }
+            }
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_chantier_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', 'Chantier mis à jour avec succès !');
+            return $this->redirectToRoute('app_chantier_index');
         }
 
         return $this->render('chantier/edit.html.twig', [
             'chantier' => $chantier,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route('/{id}', name: 'app_chantier_delete', methods: ['POST'])]
     public function delete(Request $request, Chantier $chantier, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$chantier->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $chantier->getId(), $request->request->get('_token'))) {
+            // Supprimer l'image associée
+            if ($chantier->getImage()) {
+                $imagePath = $this->getParameter('images_directory') . '/' . $chantier->getImage();
+                if (file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+            }
+
             $entityManager->remove($chantier);
             $entityManager->flush();
+
+            $this->addFlash('success', 'Chantier supprimé avec succès !');
         }
 
-        return $this->redirectToRoute('app_chantier_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_chantier_index');
     }
 }
